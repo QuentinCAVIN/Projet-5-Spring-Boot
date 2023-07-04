8442d7eaf50608fd5827b76cb139d055fa9e20e0
Quentin CAVIN <quentin.cavin@gmail.com>
2023-07-04 15:00:34 +0200

    Implémentation du squelette des méthodes d'EmergencyListController

---
+++ src/main/java/com/safetynet/alerts/service/EmergencyListService.java
@@ -0,0 +1,4 @@
+package com.safetynet.alerts.service;
+
+public interface EmergencyListService {
+}
---
+++ src/main/java/com/safetynet/alerts/service/EmergencyListServiceImpl.java
@@ -0,0 +1,4 @@
+package com.safetynet.alerts.service;
+
+public class EmergencyListServiceImpl implements EmergencyListService {
+}
