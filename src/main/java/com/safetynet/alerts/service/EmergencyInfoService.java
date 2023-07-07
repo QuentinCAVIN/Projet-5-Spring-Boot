601c6d5d9e5e764c525668134d986b80f4c25552
Quentin CAVIN <quentin.cavin@gmail.com>
2023-07-07 17:16:08 +0200

    Construction des Map endpointExpected transférés au controller + découpage des méthodes de EmergencyInfoService.

--- src/main/java/com/safetynet/alerts/service/EmergencyInfoService.java
+++ src/main/java/com/safetynet/alerts/service/EmergencyInfoService.java
@@ -7,8 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 public interface EmergencyInfoService {
-    public Map <String,Object> findEmergencyInfoOfPeopleCoveredByFirestation(Integer station);
-    public Map<String,Object> findEmergencyinfoOfCHildrenByAddress(String address);
+    public List <EmergencyInfo> findEmergencyInfoOfPeopleCoveredByFirestation(Integer station);
+    public int numberOfAdultCoveredByFirestation (Integer station);
+    public int numberOfChildrenCoveredByFirestation (Integer station);
+    public List<EmergencyInfo> findEmergencyInfoOfPeopleByAddress(String address);
+    public List<EmergencyInfo>findEmergencyinfoOfCHildrenByAddress(String address);
+    public List<String> findAdultByAddress(String address);
     public List<EmergencyInfo> getAllEmergencyInfo();
 
     public List <EmergencyInfo> getEmergencyInfoByAddress(String address);
--- src/main/java/com/safetynet/alerts/service/EmergencyInfoServiceImpl.java
+++ src/main/java/com/safetynet/alerts/service/EmergencyInfoServiceImpl.java
@@ -21,61 +21,88 @@ public class EmergencyInfoServiceImpl implements EmergencyInfoService {
     EmergencyInfoRepository emergencyInfoRepository;
 
     //TODO: Placer les findEmergency dans le repository ou dans le service?
-    public Map <String,Object> findEmergencyInfoOfPeopleCoveredByFirestation(Integer station){
-        int numberOfChilden = 0;
-        int numberOfAdult = 0;
+    public List<EmergencyInfo> findEmergencyInfoOfPeopleCoveredByFirestation(Integer station) {
 
-        List<EmergencyInfo>emergencyInfoOfPeopleCoveredByFirestation = new ArrayList<>();
-        for (EmergencyInfo emergencyInfo: getAllEmergencyInfo()) {
-            if(emergencyInfo.getStation() == station){
+        List<EmergencyInfo> emergencyInfoOfPeopleCoveredByFirestation = new ArrayList<>();
+        for (EmergencyInfo emergencyInfo : getAllEmergencyInfo()) {
+            if (emergencyInfo.getStation() == station) {
                 emergencyInfoOfPeopleCoveredByFirestation.add(emergencyInfo);
-                if (emergencyInfo.getAge() < 19){
-                    numberOfChilden ++;
-                } else {
-                    numberOfAdult ++;
-                }
             }
         }
-
-        Map <String,Object> endpointExpected = new LinkedHashMap<>();
-        endpointExpected.put("Personnes couvertes par le centre de secours n°" + station + ":", emergencyInfoOfPeopleCoveredByFirestation);
-        endpointExpected.put("Nombre d'adultes présent:",numberOfAdult);
-        endpointExpected.put("Nombre d'enfants présent:",numberOfChilden);
-
-        return endpointExpected;
+        return emergencyInfoOfPeopleCoveredByFirestation;
     }
 
-    public Map<String,Object> findEmergencyinfoOfCHildrenByAddress(String address){
-
-        List<EmergencyInfo>emergencyInfoOfChildAtThisAddress = new ArrayList<>();
-        List<String> adultAtThisAddress = new ArrayList<>();
-        for (EmergencyInfo emergencyInfo:getAllEmergencyInfo()) {
-            if(emergencyInfo.getAddress().equals(address)) {
-                if (emergencyInfo.getAge() < 19) {
-                    emergencyInfoOfChildAtThisAddress.add(emergencyInfo);
-                } else {
-                    if (!adultAtThisAddress.contains(emergencyInfo.getFirstName() + " " + emergencyInfo.getLastName()))
-                    adultAtThisAddress.add(emergencyInfo.getFirstName() + " " + emergencyInfo.getLastName());
-                }
+    public List<EmergencyInfo> findEmergencyInfoOfPeopleByAddress(String address) {
+        List<EmergencyInfo> emergencyInfoOfPeopleAtThisAddress = new ArrayList<>();
+        for (EmergencyInfo emergencyInfo : getAllEmergencyInfo()) {
+            if (emergencyInfo.getAddress().equals(address)) {
+                    emergencyInfoOfPeopleAtThisAddress.add(emergencyInfo);
             }
         }
+        return emergencyInfoOfPeopleAtThisAddress;
+    }
 
-        Map <String,Object> endpointExpected = new LinkedHashMap<>();
+    public List<EmergencyInfo> findEmergencyinfoOfCHildrenByAddress(String address) {
+
+        List<EmergencyInfo> emergencyInfoOfChildrenAtThisAddress = new ArrayList<>();
+        for (EmergencyInfo emergencyInfo : findEmergencyInfoOfPeopleByAddress(address)) {
+                if (emergencyInfo.getAge() < 19) {
+                    emergencyInfoOfChildrenAtThisAddress.add(emergencyInfo);
+                }
+                    /*if (!adultAtThisAddress.contains(emergencyInfo.getFirstName() + " " + emergencyInfo.getLastName()))
+                        adultAtThisAddress.add(emergencyInfo.getFirstName() + " " + emergencyInfo.getLastName());*/
+        }
+        return emergencyInfoOfChildrenAtThisAddress;
+    }
+
+        /*Map <String,Object> endpointExpected = new LinkedHashMap<>();
         endpointExpected.put("Enfants présents au " + address + ":", emergencyInfoOfChildAtThisAddress);
         endpointExpected.put("Adultes présents à cette adresse:",adultAtThisAddress);
 
-        return endpointExpected;
+        return endpointExpected;*/
+
+
+    public List<String> findAdultByAddress(String address) {
+        List<String> adultAtThisAddress = new ArrayList<>();
+        for (EmergencyInfo emergencyInfo : findEmergencyInfoOfPeopleByAddress(address)) {
+            if (emergencyInfo.getAge() > 18) {
+                adultAtThisAddress.add(emergencyInfo.getFirstName() + " " + emergencyInfo.getLastName());
+            }
+        }
+        return adultAtThisAddress;
     }
 
-    public List <EmergencyInfo> getAllEmergencyInfo(){
-       return emergencyInfoRepository.getListOfAllEmergencyInfo();
+    public int numberOfAdultCoveredByFirestation(Integer station) {
+        int numberOfAdult = 0;
+
+        for (EmergencyInfo emergencyInfo : findEmergencyInfoOfPeopleCoveredByFirestation(station)) {
+            if (emergencyInfo.getAge() > 18) {
+                numberOfAdult++;
+            }
+        }
+        return numberOfAdult;
     }
 
-    public List <EmergencyInfo> getEmergencyInfoByAddress(String address){
+    public int numberOfChildrenCoveredByFirestation(Integer station) {
+        int numberOfChildren = 0;
+
+        for (EmergencyInfo emergencyInfo : findEmergencyInfoOfPeopleCoveredByFirestation(station)) {
+            if (emergencyInfo.getAge() < 19) {
+                numberOfChildren++;
+            }
+        }
+        return numberOfChildren;
+    }
+
+    public List<EmergencyInfo> getAllEmergencyInfo() {
+        return emergencyInfoRepository.getListOfAllEmergencyInfo();
+    }
+
+    public List<EmergencyInfo> getEmergencyInfoByAddress(String address) {
         return emergencyInfoRepository.findAllByAddress(address);
     }
 
-    public List <EmergencyInfo> getEmergencyInfoByStation(Integer station){
+    public List<EmergencyInfo> getEmergencyInfoByStation(Integer station) {
         return emergencyInfoRepository.findAllByStation(station);
     }
 }
